5f4938a210851fe780d7ea41713064ccd65c46b4
saarock <saarock4646@gmail.com>
2025-12-03 14:00:58 +0545

    solved many errors

--- server/routers/user.router.js
+++ server/routers/user.router.js
@@ -1,4 +1,7 @@
-
-
+import express from "express";
+import { sendMailToTheUser } from "../controller/user.controller";
+const router = express.Router();
 
 router.post("/send_mail", sendMailToTheUser);
+
+export default router;
